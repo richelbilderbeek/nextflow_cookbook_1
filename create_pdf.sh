9d8b7d0fe20bcfbd0f6f372b413f14b12393035c
richelbilderbeek <richel@richelbilderbeek.nl>
2023-04-09 12:42:16 +0200

    Can create PDF

---
+++ create_pdf.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+# Create the PDF 'nextflow_cookbook_1.pdf'
+#
+# It does so by converting the TeX to PDF, 
+# as well as stitching some other PDFs to it
+#
+# Usage:
+#
+#   ./create.sh
+#
+rm -f nextflow_cookbook_1.pdf
+
+pdflatex nextflow_cookbook_1.tex
+bibtex nextflow_cookbook_1 >/dev/null
+pdflatex nextflow_cookbook_1.tex >/dev/null
+pdflatex nextflow_cookbook_1.tex
